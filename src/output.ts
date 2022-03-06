e2dd9ccb8a70326a43a718929e99009f6fa47398
Micheal Wass <michealw@users.noreply.github.com>
2022-03-05 21:11:26 -0500

    Reduced code duplication in output

--- src/output.ts
+++ src/output.ts
@@ -3,6 +3,19 @@ import Table from 'cli-table';
 import wv from './weights';
 
 export default function outputWeights(value: number, mtowMode: boolean) {
+    function calculateAndDisplayValue(val: number, weight: number): string {
+        const calc = val / weight;
+        let display = '';
+    
+        if (calc > 1.0) {
+            display = chalk.yellow(calc.toFixed(2));
+        } else {
+            display = calc.toFixed(2);
+        }
+    
+        return display;
+    }
+
     let table = new Table({
         head: ['Type', 'Payload Weight', 'Calculation']
     });
@@ -22,27 +35,9 @@ export default function outputWeights(value: number, mtowMode: boolean) {
         const mtow = weights[1];
     
         if (mtowMode) {
-            const calc = value / mtow;
-            let display = '';
-
-            if (calc > 1.0) {
-                display = chalk.yellow(calc.toFixed(2));
-            } else {
-                display = calc.toFixed(2);
-            }
-
-            table.push([type, Intl.NumberFormat('en-US').format(mtow), display]);
+            table.push([type, Intl.NumberFormat('en-US').format(mtow), calculateAndDisplayValue(value, mtow)]);
         } else {
-            const calc = value / pw;
-            let display = '';
-
-            if (calc > 1.0) {
-                display = chalk.yellow(calc.toFixed(2));
-            } else {
-                display = calc.toFixed(2);
-            }
-
-            table.push([type, Intl.NumberFormat('en-US').format(pw), display]);
+            table.push([type, Intl.NumberFormat('en-US').format(pw), calculateAndDisplayValue(value, pw)]);
         }
     }
 
